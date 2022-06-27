083f12d9844c1599b0af03db3f50a8c65368a8ba
Theo Fornetti <theoforentti2001@gmail.com>
2022-06-27 17:11:53 -0300

    Ejemplo tipo pila

---
+++ Ejemplo tipo pila/Ejemplo tipo pila/CNodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_tipo_pila
+{
+
+
+    internal class CNodo
+    {
+        //En esta variable colocamos los datos que guarda el nodo 
+        private char dato;
+
+        //Esta variable de referencia es utilizada para apuntar al nodo siguiente 
+        private CNodo siguiente = null;
+
+        //Propiedades 
+        public char Dato { get => dato; set => dato = value; }
+        internal CNodo Siguiente { get => siguiente; set => siguiente = value; }
+
+        //Para su facil Impresion 
+        public override string ToString()
+        {
+            return String.Format("[{0}]", dato);
+        }
+    }
+}
---
+++ Ejemplo tipo pila/Ejemplo tipo pila/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_tipo_pila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expresion = "";
+            char s = ' ';
+            CStack pila = new CStack();
+
+            //Pedimos la expresion a evaluar 
+            Console.WriteLine("Dame la expresion a evaluar");
+            expresion=Console.ReadLine();
+
+            foreach (char c in expresion) { 
+                 
+                //Verificamos que sea simbolo de apertura 
+                if (c=='('  || c=='{' || c == '[')
+                {
+                    pila.Push(c);
+                }
+
+                //verificamos que sea simbolo de cierre
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (pila.Vacio())
+                    {
+                        Console.WriteLine("Exceso de simbolos de cierre");
+                    }
+                    else {
+                        //Obtenemos el caracter correspondiente 
+                        s = pila.Pop();
+
+                        if (s == '(' && c != ')') {
+                            Console.WriteLine("Se esperaba )");
+                        }
+                        if (s == '[' && c != ']')
+                        {
+                            Console.WriteLine("Se esperaba ]");
+                        }
+                        if (s == '{' && c != '}')
+                        {
+                            Console.WriteLine("Se esperaba }");
+                        }
+                    }
+                }
+
+            }
+            if (pila.Vacio() == false) {
+                Console.WriteLine("Exceso de simbolos de apertura");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
